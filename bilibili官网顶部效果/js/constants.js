73b836509beae171f1fb56ca8ca652d199e0d7ab
xiaojianjin <xiaojianjin@luckyxp.net>
2024-10-14 18:39:13 +0800

    feat: update

--- bilibili官网顶部效果/js/constants.js
+++ bilibili官网顶部效果/js/constants.js
@@ -1,83 +1,112 @@
-import { PUBLIC_PATH } from './env.js'
+import { PUBLIC_PATH } from "./env.js";
 
 export const SLICE_LIST = [
   {
-    img: PUBLIC_PATH + 'images/1.webp',
-    custom: { a: 1 }
+    img: PUBLIC_PATH + "images/1.webp",
   },
   {
-    img: PUBLIC_PATH + 'images/2.webp',
+    img: PUBLIC_PATH + "images/2.webp",
   },
   {
-    img: PUBLIC_PATH + 'images/3.webp',
+    img: PUBLIC_PATH + "images/3.webp",
   },
   {
-    img: PUBLIC_PATH + 'images/4.webp',
+    img: PUBLIC_PATH + "images/4.webp",
   },
   {
-    img: PUBLIC_PATH + 'images/5.webp',
+    img: PUBLIC_PATH + "images/5.webp",
   },
   {
-    img: PUBLIC_PATH + 'images/6.webp',
+    img: PUBLIC_PATH + "images/6.webp",
   },
   {
-    img: PUBLIC_PATH + 'images/7.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/8.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/9.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/10.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/11.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/12.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/13.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/14.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/15.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/16.webp',
-  },
-  {
-    img: PUBLIC_PATH + 'images/17.webp',
-    style: {
-      position: 'absolute',
-      top: '10px',
-      left: '55%',
-      width: '10%',
-      height: 'auto',
+    img: PUBLIC_PATH + "images/7.webp",
+    transform: {
+      x: {
+        min: -150,
+        max: 350,
+        range: 10,
+      },
     },
-    custom: { a: 1 },
   },
   {
-    img: PUBLIC_PATH + 'images/18.webp',
+    img: PUBLIC_PATH + "images/8.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/9.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/10.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/11.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/12.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/13.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/14.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/15.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/16.webp",
+  },
+  {
+    img: PUBLIC_PATH + "images/17.webp",
     style: {
-      position: 'absolute',
-      top: '10px',
-      left: '65%',
-      width: '10%',
-      height: 'auto',
-    }
+      position: "absolute",
+      top: "10px",
+      left: "55%",
+      width: "10%",
+      height: "auto",
+    },
+    transform: {
+      x: {
+        min: -150,
+        max: 150,
+        range: 1,
+      },
+      rotate: {
+        min: -10,
+        max: 10,
+        range: 1,
+      },
+    },
   },
   {
-    img: PUBLIC_PATH + 'images/19.webp',
+    img: PUBLIC_PATH + "images/18.webp",
+    style: {
+      position: "absolute",
+      top: "10px",
+      left: "65%",
+      width: "10%",
+      height: "auto",
+    },
+    transform: {
+      x: {
+        min: -150,
+        max: 150,
+        range: 2,
+      },
+      rotate: {
+        min: 0,
+        max: 20,
+        range: 1,
+      },
+    },
   },
   {
-    img: PUBLIC_PATH + 'images/20.webp',
+    img: PUBLIC_PATH + "images/19.webp",
   },
   {
-    img: PUBLIC_PATH + 'images/21.webp',
+    img: PUBLIC_PATH + "images/20.webp",
   },
-]
+  {
+    img: PUBLIC_PATH + "images/21.webp",
+  },
+];
--- bilibili官网顶部效果/js/main.js
+++ bilibili官网顶部效果/js/main.js
@@ -1,16 +1,91 @@
-import { SLICE_LIST } from './constants.js'
-import { render, createElement } from './dom.js'
+import { SLICE_LIST } from "./constants.js";
+import { render, createElement } from "./dom.js";
 
 const dom_ul = createElement(
-  'ul',
-  { className: 'top_nav' },
-  SLICE_LIST.map(i => (
-    createElement('li', { className: 'top_nav_item' }, createElement('img', { src: i.img, style: i.style, __CUSTOM__: i.custom }))
-  ))
-)
+  "ul",
+  { className: "top_nav" },
+  SLICE_LIST.map((i) =>
+    createElement(
+      "li",
+      { className: "top_nav_item" },
+      createElement("img", {
+        src: i.img,
+        style: i.style,
+        __transform__: i.transform,
+        __change__: {},
+      })
+    )
+  )
+);
 
-dom_ul.addEventListener('mousemove', e => {
+let x = 0;
 
-})
+function mouseMoveEvent(e) {
+  const target = e.currentTarget;
+  const dom_imgList = target.querySelectorAll("img");
+  if (!dom_imgList.length) return;
 
-render(document.getElementById('root'), dom_ul)
+  const moveX = e.clientX - x;
+
+  dom_imgList.forEach((img, index) => {
+    const { __transform__ } = img;
+    if (!__transform__) return;
+
+    let translateX = "0px";
+    let rotate = "0deg";
+
+    if (__transform__.x) {
+      const { min, max, range } = __transform__.x;
+      let _moveX = moveX * range + (img.__change__.x || 0);
+      if (_moveX < min) _moveX = min;
+      else if (_moveX > max) _moveX = max;
+      translateX = `${_moveX}px`;
+      img.__change__.x = _moveX;
+    }
+
+    if (__transform__.rotate) {
+      const { min, max, range } = __transform__.rotate;
+      let angle =
+        ((moveX * target.offsetWidth) / 360) * range +
+        (img.__change__.rotate || 0);
+      if (angle < min) angle = min;
+      else if (angle > max) angle = max;
+      rotate = `${angle}deg`;
+      img.__change__.rotate = angle;
+    }
+
+    if (__transform__) {
+      console.log(
+        __transform__,
+        "moveX",
+        moveX,
+        "translateX",
+        translateX,
+        "rotate",
+        rotate
+      );
+    }
+
+    img.style.setProperty(
+      "transform",
+      "translateX(" + translateX + ") rotate(" + rotate + ")"
+    );
+  });
+
+  x = e.clientX;
+}
+
+dom_ul.addEventListener("mouseenter", (e) => {
+  x = e.clientX;
+  dom_ul.addEventListener("mousemove", mouseMoveEvent);
+});
+
+dom_ul.addEventListener("mouseleave", (e) => {
+  x = 0;
+  dom_ul.removeEventListener("mousemove", mouseMoveEvent);
+  dom_ul
+    .querySelectorAll("img")
+    .forEach((img) => img.style.setProperty("transform", ""));
+});
+
+render(document.getElementById("root"), dom_ul);
